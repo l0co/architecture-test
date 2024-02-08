82f3f277efb4f522c5d55140a29ec8e8b461432e
Lukasz Frankowski <lukasz.frankowski@zondaglobal.com>
2024-02-08 20:38:42 +0100

    architecture test

--- src/main/java/com/lifeinide/architecture/module/pizza/PizzaService.java
+++ src/main/java/com/lifeinide/architecture/module/pizza/PizzaService.java
@@ -1,6 +1,7 @@
 package com.lifeinide.architecture.module.pizza;
 
 import com.lifeinide.architecture.infra.Repository;
+import com.lifeinide.architecture.integration.PizzaManBrainDirectIntegrationService;
 import com.lifeinide.architecture.module.pizza.Pizza.PizzaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
@@ -9,13 +10,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class PizzaService {
 
-    @Autowired
-    private Repository repository;
+    @Autowired private Repository repository;
+    @Autowired private PizzaManBrainDirectIntegrationService pizzaManBrainDirectIntegrationService;
 
     public Pizza create(@NonNull PizzaBuilder builder) {
         Pizza pizza = builder.build();
         repository.save(pizza);
-        // TODOLF create real pizza, man
+        pizzaManBrainDirectIntegrationService.prepare(pizza);
         return pizza;
     }
 
