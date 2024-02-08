1a8d1d17961e2b365634cebef0b51469a29224b7
Lukasz Frankowski <lukasz.frankowski@zondaglobal.com>
2024-02-08 17:35:19 +0100

    architecture test

---
+++ src/main/java/com/lifeinide/architecture/ArchitectureTestApplication.java
@@ -0,0 +1,13 @@
+package com.lifeinide.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitectureTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lifeinide/architecture/ArchitectureTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeinide.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
