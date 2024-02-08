6ef5e44899e06ccae176138f0aba64b7718bb04a
Lukasz Frankowski <lukasz.frankowski@zondaglobal.com>
2024-02-08 19:32:13 +0100

    architecture test

--- src/main/java/com/lifeinide/architecture/module/user/UserService.java
+++ src/main/java/com/lifeinide/architecture/module/user/UserService.java
@@ -1,5 +1,8 @@
 package com.lifeinide.architecture.module.user;
 
+import com.lifeinide.architecture.infra.Repository;
+import com.lifeinide.architecture.module.user.User.UserBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +12,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    public User create(@NonNull String name) {
-        return User.builder()
-            .name(name)
-            .build();
+    @Autowired private Repository repository;
+
+    public User create(@NonNull UserBuilder builder) {
+        User user = builder.build();
+        repository.save(user);
+        return user;
     }
 
 }
